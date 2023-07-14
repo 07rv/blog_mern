9a499934f7fc93b62e36da0adac782163bdef185
Rohit Verma <28vrohit@gmail.com>
2023-07-14 23:42:52 +0530

    add more

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
-import Header from "./Header";
-import Post from "./Post";
+
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <main>
-      <Header />
-      <Post />
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
