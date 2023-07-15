ff8c81cfaa841f2452d46c6618ac9ba9c1cc488f
Rohit Verma <28vrohit@gmail.com>
2023-07-15 15:01:10 +0530

    edit post

--- client/src/Post.js
+++ client/src/Post.js
@@ -5,18 +5,18 @@ const Post = ({ _id, title, summary, cover, content, createdAt, author }) => {
   return (
     <div className="post">
       <div className="image">
-        <a href="/">
+        <Link to={`/post/${_id}`}>
           <img src={"http://localhost:4000/" + cover} alt="" />
-        </a>
+        </Link>
       </div>
       <div className="texts">
-        <a href={`/`}>
+        <Link to={`/post/${_id}`}>
           <h2>{title}</h2>
-        </a>
+        </Link>
         <p className="info">
-          <a href={`/`} className="author">
+          <Link href={`/`} className="author">
             {author.username}
-          </a>
+          </Link>
           <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
         <p className="summary">{summary}</p>
