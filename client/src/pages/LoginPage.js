dd9d66dcc95709688fc4e4c742e1d2a0892bf533
Rohit Verma <28vrohit@gmail.com>
2023-07-15 09:47:52 +0530

    call api

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
-  async function login(ev) {}
+  async function login(ev) {
+    ev.preventDefault();
+    const response = await fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+    if (response.ok) {
+      response.json().then((userInfo) => {
+        // setUserInfo(userInfo);
+        setRedirect(true);
+      });
+    } else {
+      alert("wrong credentials");
+    }
+  }
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <form className="login" onSubmit={login}>
       <h1>Login</h1>
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -4,7 +4,19 @@ const RegisterPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  async function register(ev) {}
+  async function register(ev) {
+    ev.preventDefault();
+    const response = await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.status === 200) {
+      alert("registration successful");
+    } else {
+      alert("registration failed");
+    }
+  }
   return (
     <form className="register" onSubmit={register}>
       <h1>Register</h1>
