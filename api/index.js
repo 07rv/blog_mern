dfc5f39f7689ca6445796906ae0b7d0e04d2b1fa
Rohit Verma <28vrohit@gmail.com>
2023-07-16 13:22:38 +0530

    limit to 10 post

--- api/index.js
+++ api/index.js
@@ -143,7 +143,7 @@ app.get("/post", async (req, res) => {
     await Post.find()
       .populate("author", ["email", "firstname", "lastname"])
       .sort({ createdAt: -1 })
-      .limit(20)
+      .limit(10)
   );
 });
 
