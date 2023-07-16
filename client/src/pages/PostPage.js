8755d03b52116ec3e063b521e96a08499539ee51
Rohit Verma <28vrohit@gmail.com>
2023-07-16 10:27:11 +0530

    post page done

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -4,6 +4,18 @@ import { formatISO9075 } from "date-fns";
 import { UserContext } from "../UserContext";
 import { Link } from "react-router-dom";
 
+import {
+  Card,
+  Button,
+  Typography,
+  CardMedia,
+  CardActions,
+  CardContent,
+} from "@mui/material";
+
+import EditIcon from "@mui/icons-material/Edit";
+import ShareIcon from "@mui/icons-material/Share";
+
 const PostPage = () => {
   const [postInfo, setPostInfo] = useState(null);
   const { userInfo } = useContext(UserContext);
@@ -18,42 +30,46 @@ const PostPage = () => {
 
   if (!postInfo) return "";
   return (
-    <div className="post-page">
-      <h1>{postInfo.title}</h1>
-      <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
-      <div className="author">by @{postInfo.author.username}</div>
-      {userInfo.id === postInfo.author._id && (
-        <div className="edit-row">
-          <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-              />
-            </svg>
-            Edit this post
-          </Link>
-        </div>
-      )}
-      <div className="image">
-        <img
-          src={`${process.env.REACT_APP_SERVER_URI}/${postInfo.cover}`}
-          alt=""
+    <>
+      <Card sx={{ maxWidth: "70%", m: "auto", mt: 3, mb: 5 }}>
+        <CardMedia
+          sx={{ height: 350 }}
+          image={`${process.env.REACT_APP_SERVER_URI}/${postInfo.cover}`}
+          title="green iguana"
         />
-      </div>
-      <div
-        className="content"
-        dangerouslySetInnerHTML={{ __html: postInfo.content }}
-      />
-    </div>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {postInfo.title}
+          </Typography>
+          <Typography gutterBottom variant="body2" color="text.secondary">
+            {formatISO9075(new Date(postInfo.createdAt))}
+          </Typography>
+          <Typography gutterBottom variant="body2" color="text.secondary">
+            {postInfo.author.firstname} {postInfo.author.lastname}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            paragraph
+            dangerouslySetInnerHTML={{ __html: postInfo.content }}
+          />
+        </CardContent>
+        <CardActions>
+          {userInfo.id === postInfo.author._id && (
+            <Button
+              href={`/edit/${postInfo._id}`}
+              endIcon={<EditIcon />}
+              sx={{ textTransform: "none" }}
+            >
+              {"Edit this Post"}
+            </Button>
+          )}
+
+          <Button endIcon={<ShareIcon />} sx={{ textTransform: "none" }}>
+            {"Share"}
+          </Button>
+        </CardActions>
+      </Card>
+    </>
   );
 };
 
